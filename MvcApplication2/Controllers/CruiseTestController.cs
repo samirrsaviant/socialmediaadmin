52c162b8ef3fce59e2fbc42feaf78f6258bf7858
samirrsaviant <samir@saviantconsulting.com>
2013-12-05 14:41:38 +0530

    CruiseTest Controller Added

---
+++ MvcApplication2/Controllers/CruiseTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class CruiseTestController : Controller
+    {
+        //
+        // GET: /CruiseTest/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
