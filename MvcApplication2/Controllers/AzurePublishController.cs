036ed0462ec066a9bcc9350f574b2df3113b71c9
samirrsaviant <samir@saviantconsulting.com>
2013-12-05 18:20:44 +0530

    AzurePublish

---
+++ MvcApplication2/Controllers/AzurePublishController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class AzurePublishController : Controller
+    {
+        //
+        // GET: /AzurePublish/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
