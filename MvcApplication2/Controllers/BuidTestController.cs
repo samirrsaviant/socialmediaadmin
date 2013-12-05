bade3a053b2b8f145df2a86f6c8bfe2bb716fd59
samirrsaviant <samir@saviantconsulting.com>
2013-12-05 16:21:49 +0530

    BuildTest Controller Add

---
+++ MvcApplication2/Controllers/BuidTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class BuidTestController : Controller
+    {
+        //
+        // GET: /BuidTest/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
