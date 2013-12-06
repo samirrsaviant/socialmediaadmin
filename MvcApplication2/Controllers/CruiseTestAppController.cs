8c3f6f2b10ae39db497fa2645c1877249bf406cd
samirrsaviant <samir@saviantconsulting.com>
2013-12-06 19:10:48 +0530

    CruiseTestAppController

---
+++ MvcApplication2/Controllers/CruiseTestAppController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class CruiseTestAppController : Controller
+    {
+        //
+        // GET: /CruiseTestApp/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
